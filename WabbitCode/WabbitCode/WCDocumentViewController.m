a1489342ac021ca17256a24ebad2a2b99d693089
William Towe <williamtowe@Williams-MacBook-Pro.local>
2014-07-29 05:00:53 -0400

    get rid of cocoapods use submodules for all dependencies

---
+++ WabbitCode/WabbitCode/WCDocumentViewController.m
@@ -0,0 +1,60 @@
+//
+//  WCDocumentViewController.m
+//  WabbitStudio
+//
+//  Created by William Towe on 7/28/14.
+//  Copyright (c) 2014 William Towe, LLC. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#import "WCDocumentViewController.h"
+#import <WCFoundation/WCPlainTextFile.h>
+#import "WCPlainTextViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
+#import <ReactiveCocoa/EXTScope.h>
+
+@interface WCDocumentViewController ()
+@property (weak,nonatomic) WCFile *file;
+
+@property (strong,nonatomic) WCBaseViewController *contentViewController;
+@end
+
+@implementation WCDocumentViewController
+
+- (void)loadView {
+    [super loadView];
+    
+    if ([self.file isKindOfClass:[WCPlainTextFile class]]) {
+        [self setContentViewController:[[WCPlainTextViewController alloc] initWithPlainTextFile:(WCPlainTextFile *)self.file]];
+        [self.view addSubview:self.contentViewController.view];
+        [self.contentViewController.view setFrame:self.view.bounds];
+    }
+    
+    @weakify(self);
+    
+    [[[[NSNotificationCenter defaultCenter]
+       rac_addObserverForName:NSViewFrameDidChangeNotification object:self.view]
+      takeUntil:[self rac_willDeallocSignal]]
+     subscribeNext:^(id _) {
+         @strongify(self);
+         
+         [self.contentViewController.view setFrame:self.view.bounds];
+    }];
+}
+
+- (instancetype)initWithFile:(WCFile *)file; {
+    if (!(self = [super init]))
+        return nil;
+    
+    NSParameterAssert(file);
+    
+    [self setFile:file];
+    
+    return self;
+}
+
+@end
---
+++ WabbitCode/WabbitCodeTests/WabbitCodeTests.m
@@ -0,0 +1,34 @@
+//
+//  WabbitCodeTests.m
+//  WabbitCodeTests
+//
+//  Created by William Towe on 7/29/14.
+//  Copyright (c) 2014 William Towe, LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WabbitCodeTests : XCTestCase
+
+@end
+
+@implementation WabbitCodeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
