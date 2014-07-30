18addc9d8559bed79ec6684ddf0cb58e7b7fad29
William Towe <willbur1984@gmail.com>
2014-07-30 11:21:50 -0400

    added NSArray+WCExtensions

--- WCFoundation/WCFoundation/NSArray+WCExtensions.h
+++ WCFoundation/WCFoundation/NSArray+WCExtensions.h
@@ -1,8 +1,8 @@
 //
-//  WCFoundation.h
+//  NSArray+WCExtensions.h
 //  WCFoundation
 //
-//  Created by William Towe on 7/28/14.
+//  Created by William Towe on 7/30/14.
 //  Copyright (c) 2014 William Towe, LLC. All rights reserved.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -11,23 +11,10 @@
 //
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#import <WCFoundation/WCConstants.h>
-#import <WCFoundation/WCDebugging.h>
-#import <WCFoundation/WCFunctions.h>
+#import <Foundation/Foundation.h>
 
-#import <WCFoundation/NSBundle+WCExtensions.h>
-#import <WCFoundation/NSArray+WCExtensions.h>
+@interface NSArray (WCExtensions)
 
-#import <WCFoundation/WCFile.h>
-#import <WCFoundation/WCPlainTextFile.h>
+- (NSString *)WC_keypath;
 
-#import <WCFoundation/WCBaseWindowController.h>
-
-#import <WCFoundation/WCAboutWindowController.h>
-
-#import <WCFoundation/WCPreferencesViewController.h>
-#import <WCFoundation/WCPreferencesWindowController.h>
-
-#import <WCFoundation/WCBaseViewController.h>
-
-#import <WCFoundation/WCRulerView.h>
+@end
