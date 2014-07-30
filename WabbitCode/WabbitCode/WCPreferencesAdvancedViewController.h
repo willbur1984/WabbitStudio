b25b74694c232afb9c0f2a4819aeab715914968a
William Towe <williamtowe@Williams-MacBook-Pro.local>
2014-07-30 01:44:28 -0400

    hook up preferences menu item

--- WabbitCode/WabbitCode/WCPreferencesAdvancedViewController.h
+++ WabbitCode/WabbitCode/WCPreferencesAdvancedViewController.h
@@ -1,8 +1,8 @@
 //
-//  WCAppDelegate.m
+//  WCPreferencesAdvancedViewController.h
 //  WabbitCode
 //
-//  Created by William Towe on 7/27/14.
+//  Created by William Towe on 7/30/14.
 //  Copyright (c) 2014 William Towe, LLC. All rights reserved.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -11,32 +11,9 @@
 //
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#import "WCAppDelegate.h"
-#import <WCFoundation/WCAboutWindowController.h>
-#import <WCFoundation/WCPreferencesWindowController.h>
-#import "WCPreferencesGeneralViewController.h"
-#import "WCPreferencesAdvancedViewController.h"
+#import <WCFoundation/WCBaseViewController.h>
+#import <WCFoundation/WCPreferencesViewController.h>
 
-@interface WCAppDelegate ()
-- (IBAction)_aboutAction:(id)sender;
-- (IBAction)_preferencesAction:(id)sender;
-@end
-
-@implementation WCAppDelegate
-
-- (BOOL)applicationShouldOpenUntitledFile:(NSApplication *)sender {
-    return NO;
-}
-
-- (void)applicationDidFinishLaunching:(NSNotification *)note {
-    
-}
-
-- (IBAction)_aboutAction:(id)sender; {
-    [[[WCAboutWindowController alloc] init] showWindow:nil];
-}
-- (IBAction)_preferencesAction:(id)sender; {
-    [[[WCPreferencesWindowController alloc] initWithViewControllerClasses:@[[WCPreferencesGeneralViewController class],[WCPreferencesAdvancedViewController class]]] showWindow:nil];
-}
+@interface WCPreferencesAdvancedViewController : WCBaseViewController <WCPreferencesViewController>
 
 @end
