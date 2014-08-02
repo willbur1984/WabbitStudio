22103b2e25a10f6afbce91e3f14bb70a2498bf72
William Towe <willbur1984@gmail.com>
2014-08-02 00:51:28 -0400

    added NSTextView+WCExtensions

--- WCEdit/WCEdit/NSTextView+WCExtensions.h
+++ WCEdit/WCEdit/NSTextView+WCExtensions.h
@@ -1,8 +1,8 @@
 //
-//  WCEdit.h
+//  NSTextView+WCExtensions.h
 //  WCEdit
 //
-//  Created by William Towe on 8/1/14.
+//  Created by William Towe on 8/2/14.
 //  Copyright (c) 2014 William Towe, LLC. All rights reserved.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -11,19 +11,10 @@
 //
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#ifndef __WC_EDIT__
-#define __WC_EDIT__
+#import <Cocoa/Cocoa.h>
 
-#import <WCEdit/WCEditConstants.h>
-#import <WCEdit/WCEditFunctions.h>
+@interface NSTextView (WCExtensions)
 
-#import <WCEdit/NSTextView+WCExtensions.h>
+- (NSRange)WC_visibleRange;
 
-#import <WCEdit/WCPlainTextFile.h>
-
-#import <WCEdit/WCPlainTextView.h>
-#import <WCEdit/WCRulerView.h>
-
-#import <WCEdit/WCPlainTextViewController.h>
-
-#endif
+@end
